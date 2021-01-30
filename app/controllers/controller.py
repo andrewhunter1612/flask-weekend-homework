65cacb795e51a2e60f43035bfcaec57a9f7560b2
Andrew Hunter <andrewhunter1612@gmail.com>
2021-01-30 17:29:06 +0000

    done something good

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,32 +1,45 @@
 from app import app
 from flask import render_template, redirect, request
 from app.models.player import *
-from app.models.game import *
+from app.models.game import Game
+from app.models.set_up_game import game, player_one, player_two
 
+def set_up():
+    game = Game()
+    player_one = Player()
+    player_two = Player()
 
-@app.route('/home')
+@app.route('/')
 def home_screen():
     return render_template("home_page.html")
 
-@app.route('/home', methods=["POST"])
+@app.route('/', methods=["POST"])
 def choose_number_of_players():
-    game = Game()
     game.set_human_players(request.form['number_of_players'])
     number_of_players = game.get_human_players()
-    player_one = Player()
-    player_two = Player()
     for _ in range(3):
         if number_of_players == 1:
             player_one = Player(True, "")
         elif number_of_players == 2:
             player_one = Player(True, "")
             player_two = Player(True, "")
+
     return redirect('/input')
 
-    @app.route('/input')
-    def input_screen():
-        game = Game()
-        number_of_humans = game.get_human_players()
-        return render_template("player_input.html", players=number_of_humans)
+@app.route('/player-input', methods=["POST"])
+def set_player_input():
+    player_one.set_player_choice(request.form['player_one'])
+    player_two.set_player_choice(request.form['player_two'])
+    
+
+    return redirect("/")
+
+@app.route('/results')
+def results_screen():
+    pass
+
+@app.route('/input')
+def input_screen():
+    return render_template("player_input.html", players=game)
 
 
---
+++ app/models/set_up_game.py
@@ -0,0 +1,7 @@
+from app.models.game import Game
+from app.models.player import Player
+
+game = Game()
+player_one = Player()
+player_two = Player()
+
